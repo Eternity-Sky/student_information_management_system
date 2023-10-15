350de4637ff0a07b7f2c4fcac79e1d6cd5c2ef9d
Leaovo-man2 <samsayamy@outlook.com>
2023-10-15 13:26:52 +0800

    add

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 import csv
+import os
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter import simpledialog
+
+data = []
 
 
 def load_data():
@@ -16,11 +19,10 @@ def load_data():
 
 
 def save_data(data):
-    filename = asksaveasfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
-    if filename:
-        with open(filename, "w") as file:
-            json.dump(data, file, indent=4)
-        messagebox.showinfo("成功", "数据保存成功！")
+    filename = "load/data.json"
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+    messagebox.showinfo("成功", "数据保存成功！")
 
 
 def export_csv(data):
@@ -49,7 +51,7 @@ def add_student():
 
 
 def search_student():
-    query = simpledialog.askstring("查找学生信息", "请输入学生姓名：")
+    query = simpledialog.askstring("查找学生信息", "请输入查询条件：")
     if not query:
         return
     result = []
@@ -63,42 +65,48 @@ def search_student():
 
 
 def about():
-    messagebox.showinfo("关于", "这是一个学生信息管理系统\n作者：Leaovo-man2\n开源地址：https://github.com/Leaovo-man2"
-                                "/student_information_management_system")
+    messagebox.showinfo("关于", "这是一个学生信息管理系统\n作者：Leaovo-man2\n开源地址：https://github.com/Leaovo-man2/student_information_management_system/")
 
 
 def main():
+    if not os.path.isdir("load"):
+        os.mkdir("load")
+
+    data_file = "load/data.json"
+    if os.path.isfile(data_file):
+        with open(data_file, "r") as file:
+            data = json.load(file)
+        messagebox.showinfo("成功", "数据加载成功！")
+    else:
+        data = []
+
     root = Tk()
     root.title("学生信息管理系统")
 
-    # 创建提示文本
-    prompt_label = Label(root, text="请在菜单栏上操作", font=("Arial", 16))
-    prompt_label.pack(pady=50)
-
     menubar = Menu(root)
-
     file_menu = Menu(menubar, tearoff=0)
-    file_menu.add_command(label="加载数据", command=lambda: load_data())
-    file_menu.add_command(label="保存数据", command=lambda: save_data(data))
-    file_menu.add_command(label="导出为CSV", command=lambda: export_csv(data))
+    file_menu.add_command(label="导出CSV", command=lambda: export_csv(data))
     file_menu.add_separator()
-    file_menu.add_command(label="退出", command=root.quit)
+    file_menu.add_command(label="退出", command=lambda: save_and_exit(root, data))
     menubar.add_cascade(label="文件", menu=file_menu)
 
     edit_menu = Menu(menubar, tearoff=0)
-    edit_menu.add_command(label="添加学生信息", command=add_student)
-    edit_menu.add_command(label="查找学生信息", command=search_student)
+    edit_menu.add_command(label="添加学生", command=lambda: add_student())
+    edit_menu.add_command(label="查找学生", command=lambda: search_student())
     menubar.add_cascade(label="编辑", menu=edit_menu)
 
     help_menu = Menu(menubar, tearoff=0)
-    help_menu.add_command(label="关于", command=about)
+    help_menu.add_command(label="关于", command=lambda: about())
     menubar.add_cascade(label="帮助", menu=help_menu)
 
     root.config(menu=menubar)
     root.mainloop()
 
 
-data = [{}]
+def save_and_exit(root, data):
+    save_data(data)
+    root.destroy()
+
 
 if __name__ == "__main__":
     main()
